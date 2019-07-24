7c16e6b411fe8214be6be9d6e7a99b2333a1ae77
zhaoyuechao <542610415@qq.com>
2019-07-24 21:26:38 +0800

    调整到能正常启动

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -4,8 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class SpringbootdemoApplication {
     private static final Logger LOGO = LoggerFactory.getLogger(SpringbootdemoApplication.class);
     public static void main(String[] args) {
